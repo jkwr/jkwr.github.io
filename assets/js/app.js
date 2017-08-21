ae7aa0e79a876743fc7d02a66f28159f80055c6d
Jevell Rollins <jkwr92@gmail.com>
2017-08-21 08:39:33 -0700

    This is for more pages that have been added + the HW of push img with arr

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,37 @@
 // sanity check
 console.log("app.js linked.");
+
+let images = [
+{
+	'name': 'Dice Game',  	
+	'url': 'https:jkwr.github.io/Dicegame.io/', 
+	'images': 'assets/imgs/dicegame.jpg' 
+
+}
+];
+
+$(document).ready(function () {
+//  var n = [];
+// 	for (let i = 0; i < images.length; i++ ) {
+// 		n.push(images[i]);
+// 	}
+// 	return n ;
+// });
+
+
+for (let i=0;i<images.length;i++) {
+	$('#page3').append(`<img id="dg" src=${images[0].images}>`);
+}
+});
+
+// $(‘.port’).eq(0).append(`<img src=${portfolio[0].img}>`);
+
+ $('.pushpin-demo-nav').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+        
